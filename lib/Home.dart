15ca5d150974c62b2a8373fdfa35444a17baa8a4
dvalerio <dnotms290>
2019-10-29 13:33:51 -0300

    Tela empresa

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,3 +1,7 @@
+import 'package:atm_consultoria_flutter_app/TelaCliente.dart';
+import 'package:atm_consultoria_flutter_app/TelaContato.dart';
+import 'package:atm_consultoria_flutter_app/TelaEmpresa.dart';
+import 'package:atm_consultoria_flutter_app/TelaServico.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -7,7 +11,27 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   void _abrirEmpresa() {
+    Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => TelaEmpresa()));
+  }
 
+  void _abrirServico() {
+    Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => TelaServico()));
+  }
+
+  void _abrirCliente() {
+    Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => TelaCliente()));
+  }
+
+  void _abrirContato() {
+    Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => TelaContato()));
   }
 
   @override
@@ -36,7 +60,7 @@ class _HomeState extends State<Home> {
                       child: Image.asset("imagens/menu_empresa.png"),
                     ),
                     GestureDetector(
-                      onTap: () => _abrirEmpresa(),
+                      onTap: () => _abrirServico(),
                       child: Image.asset("imagens/menu_servico.png"),
                     )
                   ],
@@ -49,11 +73,11 @@ class _HomeState extends State<Home> {
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
                     GestureDetector(
-                      onTap: () => _abrirEmpresa(),
+                      onTap: () => _abrirCliente(),
                       child: Image.asset("imagens/menu_cliente.png"),
                     ),
                     GestureDetector(
-                      onTap: () => _abrirEmpresa(),
+                      onTap: () => _abrirContato(),
                       child: Image.asset("imagens/menu_contato.png"),
                     )
                   ],
